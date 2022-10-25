d0ef118185c458739a1bd5b85d9b6111565ff708
malhamokhtari <111632549+malhamokhtari@users.noreply.github.com>
2022-10-25 11:48:12 +0200

    Update affichage.java

--- affichage.java
+++ affichage.java
@@ -1,5 +1,5 @@
 class affichage{
         public static void main(String args[]){
-                System.out.println("Hello World");
+                System.out.println("Formation Devops Goldentechnology");
         }
 }
